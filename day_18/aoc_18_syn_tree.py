64413da9a8e7d1a08baab092c712c9c310c5ea49
Frederic Bergeron <fbergeron@rocketmail.com>
2020-12-21 15:48:14 +0900

    Fixed a few issues with parenthesis parsing.

--- day_18/aoc_18_syn_tree.py
+++ day_18/aoc_18_syn_tree.py
@@ -8,6 +8,10 @@ problems = []
 with open(input_data_filename, 'r') as input_file:
     for line in input_file:
         line = line.strip()
+        if line.startswith('#'):
+            continue
+        if '#' in line:
+            line = line[:line.index('#')]
         problems.append(line.replace(' ', ''))
 print(problems)
 
@@ -37,32 +41,44 @@ def build_tree(chars):
 #         return [c, tree, build_tree_rec([], chars[1:])] 
 
 
-def build_tree_rec(tree, chars):
-    print(f"build_tree_rec tree={tree} chars={chars}")
+def build_tree_rec(tree, chars, level=0, parent_chars=[]):
+    print(f"build_tree_rec tree={tree} chars={chars} level={level} parent_chars={parent_chars}")
     if chars == None or len(chars) == 0:
         return tree
 
     c = chars[len(chars) - 1]
     
-    if c.isdigit():
-        return build_tree_rec([int(c)], chars[:-1])
+    if level == 0:
+        if c.isdigit():
+            return build_tree_rec([int(c)], chars[:-1], level)
 
-    elif c == '+':
-        return [c, build_tree_rec([], chars[:-1]), tree] 
-    elif c == '-':
-        return [c, build_tree_rec([], chars[:-1]), tree] 
-    elif c == '*':
-        return [c, build_tree_rec([], chars[:-1]), tree] 
-    elif c == '/':
-        return [c, build_tree_rec([], chars[:-1]), tree] 
-
+        elif c == '+':
+            return [c, build_tree_rec([], chars[:-1], level, parent_chars), tree] 
+        elif c == '-':
+            return [c, build_tree_rec([], chars[:-1], level, parent_chars), tree] 
+        elif c == '*':
+            return [c, build_tree_rec([], chars[:-1], level, parent_chars), tree] 
+        elif c == '/':
+            return [c, build_tree_rec([], chars[:-1], level, parent_chars), tree] 
+    if c == ')':
+        # return build_tree_rec([], chars[:-1], level + 1, parent_chars)
+        return build_tree_rec(tree, chars[:-1], level + 1, parent_chars)
+    elif c == '(':
+        print(f"( found parent_chars={parent_chars}")
+        # subtree = build_tree_rec([], parent_chars, level - 1)
+        subtree = build_tree_rec(tree, parent_chars)
+        print(f"subtree={subtree}")
+        return build_tree_rec(subtree, chars[:-1], level - 1)
+    else:
+        parent_chars = [c] + parent_chars
+        return build_tree_rec(tree, chars[:-1], level, parent_chars)
 
 def evaluate_tree_rec(tree):
     if tree is None or len(tree) == 0:
         return 0
 
     value = tree[0]
-    print(type(value))
+    # print(type(value))
     if value == '+':
         return evaluate_tree_rec(tree[1]) + evaluate_tree_rec(tree[2])
     elif value == '-':
@@ -79,7 +95,8 @@ sum = 0
 for problem in problems:
     chars = list(problem)
     tree = build_tree(chars)
-    print(f"tree={tree}")
+    print(f"tree={tree}\n\n")
     res = evaluate_tree_rec(tree)
+    print(f"res={res}\n\n")
     sum += res
 print(f"sum={sum}")
